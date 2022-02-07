f6a4e07f4e6dc08c4a9196ff580c79b88e63dbe8
MarinaCricol <marina.cricol.ext@extendaretail.com>
2022-02-07 13:41:59 +0200

    Add constants

--- src/main/java/selfStudy/CelsiusConvertToKelvin.java
+++ src/main/java/selfStudy/CelsiusConvertToKelvin.java
@@ -1,9 +1,10 @@
 package selfStudy;
 
 public class CelsiusConvertToKelvin implements BaseConverter{
+	private static final double KELVIN_CONSTANT=273.15;
 	
 	public double converter(double celsiusToK) {
-		double kelvin = celsiusToK + 273.15;
+		double kelvin=celsiusToK + KELVIN_CONSTANT;
 		return kelvin;
 	}
 }
--- src/main/java/selfStudy/CelsiusConverterToFahrenheit.java
+++ src/main/java/selfStudy/CelsiusConverterToFahrenheit.java
@@ -1,9 +1,10 @@
 package selfStudy;
 
 public class CelsiusConverterToFahrenheit implements BaseConverter {
+	private static final double FAHRENHEIT_CONSTANT = 32;
 	
 	public double converter(double celsiusToF) {
-		double fahrenheit = (celsiusToF * 9/5) + 32;
+		double fahrenheit = (celsiusToF * 9/5) + FAHRENHEIT_CONSTANT;
 		return fahrenheit;
 	}
 }
